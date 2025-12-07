30aa752724fbdb5bf49e07ba584330e6a63a1762
Johan Mendez <punkeneto@hotmail.com>
2025-12-06 20:08:30 -0500

    M001 test configs added

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: ['./__tests__/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        '__tests__/',
+        'dist/',
+        '*.config.ts',
+        '*.config.js',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
